f80de0f3cdc216b74dbedb57a9ef0e167bfa97a6
Lachlan Grant <lachlangrant@rbvea.co>
2017-02-19 08:38:41 +1000

    Initial Commit

---
+++ ConvertKit_iOS/ConvertKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ConvertKit_iOS.h
+//  ConvertKit_iOS
+//
+//  Created by Lachlan Grant on 19/2/17.
+//  Copyright © 2017 Lachlan Grant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConvertKit_iOS.
+FOUNDATION_EXPORT double ConvertKit_iOSVersionNumber;
+
+//! Project version string for ConvertKit_iOS.
+FOUNDATION_EXPORT const unsigned char ConvertKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvertKit_iOS/PublicHeader.h>
+
+
